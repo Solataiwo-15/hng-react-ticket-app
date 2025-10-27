9f7c3ea29f416435aa24395370eaf25d05c0d39b
Ahmad Taiwo <ahmadtaiwo15@gmail.com>
2025-10-27 08:58:20 +0100

    Vercel Deployment Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import singleSpa from 'vite-plugin-single-spa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), singleSpa({
+    type: 'root',
+    entry: '/src/index.jsx'
+  })],
+  
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/, // Updated to include .jsx
+    exclude: [],
+  },
 })
