704f35ba6333e9a9454a4c1f036adffc07c67d0f
Ahmad Taiwo <ahmadtaiwo15@gmail.com>
2025-10-28 20:54:37 +0100

    Readme Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
